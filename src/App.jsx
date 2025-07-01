7acc49ee425c0d8356bbbadf71d924104c8ce07b
hamabaeso <hamabaeso@gmail.com>
2025-07-01 20:35:37 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>재료 기반 음식 마인드맵 (사이트 전용)</h1>
+      <p>이곳에 마인드맵 기능이 추가될 예정입니다.</p>
+    </div>
+  );
+}
